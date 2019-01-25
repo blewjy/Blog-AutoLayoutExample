49ac0f20f0ef0195b1f97e1af69b4c3c2dbf0d75
blewjy <blewjy@gmail.com>
2019-01-26 03:11:01 +0800

    Initial Commit

---
+++ Blog-AutoLayoutExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Blog-AutoLayoutExample
+//
+//  Created by Bryan Lew on 26/1/19.
+//  Copyright © 2019 Bryan Lew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
